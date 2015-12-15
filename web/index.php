ece349edfa2ca8d8d6ddcdef1d8c97cfbadc02d1
ngraves95 <ngraves95@gmail.com>
2015-12-15 14:18:14 -0500

    Populate add button with all pids on page serve

--- web/index.php
+++ web/index.php
@@ -1,3 +1,22 @@
+<?php
+$db = new mysqli("localhost", "obdii", "obdii", "mock");
+if ($db->connect_errno) {
+    echo "Connect failed: ".$db->connect_error;
+    exit();
+ }
+$pid_query = "select pid, name, units from pid_metadata";
+$pid_result = $db->query($pid_query);
+
+if (!$pid_result) {
+    $pid_result->close();
+}
+$output = "";
+while ($pid_row = $pid_result->fetch_assoc()) {
+     $output = $output.'<option value="'.$pid_row["pid"].'" label="'
+             .$pid_row["name"].'">'.$pid_row["units"]."</option>";
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,9 +29,8 @@
     <div class="add-button" onclick="togglePidSelector()">+</div>
     <select id="pidsel" class="pid-selector" name="pids"
 	      onchange="registerPid()" >
-	<option value="" > -- Please select a PID -- </option>
-	<option value="x" label="New">rpm</option>
-	<option value="y" label="Newer">psi</option>
+                <option value=""> -- Please select a PID -- </option>
+    <?php echo $output ?>
     </select>
     <div id="content">
       <div id="content-inner">
