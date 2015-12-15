8a7bc99e6e2374a5233e427f930283b15f5a5934
ngraves95 <ngraves95@gmail.com>
2015-12-14 19:34:44 -0500

    Clean up web stuff

--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Copies all files from our local web folder to production location.
+# Remove any old data from deployment location.
+rm /var/www/*
+
+# Remove emacs backup files from web directory.
+rm ../web/*~ 2>/dev/null
+
+# Copy all files from our local web folder to production location.
 cp ../web/* /var/www/
