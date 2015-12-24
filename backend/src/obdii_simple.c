7bdbedc6616ac96a913b8b96e681b3a24b06d8d8
ngraves95 <ngraves95@gmail.com>
2015-12-23 19:10:37 -0500

    Implement communication with OBD-II device

--- backend/src/obdii_simple.c
+++ backend/src/obdii_simple.c
@@ -10,21 +10,200 @@
 #include <string.h>
 #include <time.h>
 
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+#define LOCATION "/dev/"
+#define NAME "ttyUSB0"
+#define DEVICE_NAME (LOCATION NAME)
+
+#ifdef DEBUG
+#define DEBUG_DETAIL(msg, payload) fprintf(stderr, "[%s] %s %s\n",\
+					   __func__, msg, payload)
+#define DEBUG_PRINT(msg) fprintf(stderr, "[%s] %s\n", __func__, msg)
+#define DEBUG_CODE(msg) fprintf(stderr, "[%s] Error code: %d\n",\
+				__func__, msg)
+#else
+#define DEBUG_DETAIL(msg, payload)
+#define DEBUG_PRINT(msg)
+#define DEBUG_CODE(msg)
+#endif
+
 /*
- * Used for communication between obdii_request and obdii_read.
- * This only exists for mocking purposes, and can be thought of
- * as the OBD-II device itself.
+ * The file descriptor from opening DEVICE_NAME.
+ *
+ * If file_des == 0, aka stdout, then we know the file descriptor
+ * for DEVICE_NAME has not been opened yet, and we need to.
  */
-static char last_pid_requested;
+static int file_des = 0;
+
+/*
+ * Device configuration. OBD-II reader should be:
+ * <BAUD>-8-N-1.
+ * Important: use the constants defines in termios.h
+ * Large amounts of code taken from:
+ * http://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+ */
+static int configure_device(int fd, int baud, int nbits,
+			    int parity, int stop_bits)
+{
+	struct termios tty;
+	memset(&tty, 0, sizeof(tty));
+
+	if (tcgetattr(fd, &tty) != 0) {
+		DEBUG_PRINT("Failed to get attr (tcgetattr)");
+		DEBUG_CODE(errno);
+		return -1;
+	}
+
+	/* Set baud rate. */
+	cfsetospeed(&tty, baud);
+	cfsetispeed(&tty, baud);
+
+	/* Set bits per byte */
+	tty.c_cflag &= (~CSIZE);
+	tty.c_cflag |= nbits;
+
+	/* Set control flags. This code taken directly from SO */
+	tty.c_iflag &= ~IGNBRK;         /* disable break processing */
+	tty.c_iflag &= ~(IXON | IXOFF | IXANY);  /* shut off xon/xoff ctrl */
+        tty.c_lflag = 0;                /* no signaling chars, no echo, */
+        tty.c_oflag = 0;
+
+        tty.c_cc[VMIN]  = 0;            /* read doesn't block */
+        tty.c_cc[VTIME] = 5;            /* 0.5 seconds read timeout */
+
+        tty.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls, */
+                                         /* enable reading */
+
+	/*
+	 * Set the parity. The parity argument contains both the
+	 * enable flag and, if the parity is enabled, whether the
+	 * parity is even or odd.
+	 */
+	tty.c_cflag &= ~(PARENB | PARODD);
+	tty.c_cflag |= parity;
+
+	/* Set the stop bits. Default is 1 stop bit. */
+	tty.c_cflag &= ~(CSTOPB);
+	tty.c_cflag |= stop_bits;
+
+	tty.c_cflag &= ~(CRTSCTS); /* This was from stack overflow */
+
+	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+		DEBUG_PRINT("Failed to set attr (tcsetattr)");
+		DEBUG_CODE(errno);
+		return -2;
+	}
+
+	return 0;
+}
+
+static int set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof(tty));
+
+        if (tcgetattr (fd, &tty) != 0) {
+                return - 1;
+        }
+
+        tty.c_cc[VMIN]  = !!should_block; /* Coerce to 1 or 0 */
+        tty.c_cc[VTIME] = 30;		  /* 3 second timeout */
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0) {
+		return -2;
+	}
+
+	return 0;
+}
+
+/*
+ * Configures the OBD-II device with the default set up:
+ *     38400-8-N-1
+ * Also set the device to block until the specified number of bytes are
+ * are received.
+ */
+static int configure_obdii(int fd)
+{
+	/*
+	 * Doing the structure like this is a little more verbose,
+	 * but more informative on a failure.
+	 * Failure codes in [-1, -2] correspond to configure_device.
+	 * Failure codes in [-4, -8] correspond to set_blocking.
+	 */
+	int retval = configure_device(fd, B38400, CS8, 0, 0);
+	if (retval != 0) {
+		DEBUG_PRINT("Failed to configure device.");
+		return retval;
+	}
+
+	retval = set_blocking(fd, 0);
+	return retval << 2;
+}
+
+/*
+ * Opens the OBD-II device referenced by the file path.
+ */
+obdii_result_t obdii_open(char * location)
+{
+	int temp = open(DEVICE_NAME, O_RDWR | O_NOCTTY | O_SYNC);
+	if (temp == -1) {
+		return OBDII_FAILURE;
+	}
+
+	file_des = temp;
+	return (configure_obdii(file_des) == 0)
+		? OBDII_SUCCESS : OBDII_FAILURE;
+}
+
+/*
+ * Closes the OBD-II device that was opened.
+ */
+obdii_result_t obdii_close(void)
+{
+	return (close(file_des) == 0) ? OBDII_SUCCESS : OBDII_FAILURE;
+}
 
 /*
  * Mock requester for now.
  */
 obdii_result_t obdii_request(char mode, char pid)
 {
-	/* Make compiler shutup over unused variables */
-	last_pid_requested = pid;
-	return (obdii_result_t) mode | 0x1;
+	/*
+	 * A write looks like 0100\r\n
+	 * write_buf holds an extra char for a null terminator,
+	 * which is invaluable for debugging.
+	 */
+	char write_buf[7];
+	char * cur = &write_buf[0];
+	unsigned count = sizeof(write_buf) - 1;
+
+	/* Check if we need to open the OBD-II reader. */
+	if (file_des == 0) {
+		DEBUG_PRINT("Opening device");
+		if (obdii_open(DEVICE_NAME) == OBDII_FAILURE) {
+			DEBUG_DETAIL("Failed to open", DEVICE_NAME);
+			return OBDII_FAILURE;
+		}
+		DEBUG_PRINT("Device opened");
+	}
+
+	cur += sprintf(cur, "%02d%02x\r\n", mode, ((unsigned) pid) & 0xFFU);
+	*cur = '\0';
+
+	DEBUG_PRINT("Writing to device.");
+	/* Write to device. */
+	if (write(file_des, write_buf, count) != count) {
+		DEBUG_PRINT("Failed to write enough bytes.");
+		DEBUG_DETAIL("Target:", write_buf);
+		return OBDII_FAILURE;
+	}
+
+	DEBUG_PRINT("Write succeeded.");
+	return OBDII_SUCCESS;
 }
 
 /*
@@ -32,31 +211,35 @@ obdii_result_t obdii_request(char mode, char pid)
  */
 obdii_result_t obdii_read(char * out_buffer, int * bytes_read)
 {
-	int pid = last_pid_requested;
-	int n_bytes = pid_data[pid].response_len;
-	int i;
-	char mock_data[sizeof(int) / sizeof(char)];
-
-	time_t t;
-	srand((unsigned) time(&t));
-
-	for (i = 0; i < sizeof(mock_data) / sizeof(mock_data[0]); i++) {
-		mock_data[i] = rand() & 0xFF;
+        /* Check if we need to open the OBD-II reader. */
+	if (file_des == 0) {
+		DEBUG_PRINT("Opening device");
+		if (obdii_open(DEVICE_NAME) == OBDII_FAILURE) {
+			DEBUG_DETAIL("Failed to open", DEVICE_NAME);
+			return OBDII_FAILURE;
+		}
 	}
 
-	char * cur = out_buffer;
+	DEBUG_PRINT("Reading from device.");
 
-	cur += sprintf(cur, "%d %x", 41, pid);
-
-	for(i = 0; i < n_bytes && i < (int)sizeof(mock_data); i++) {
-		cur += sprintf(cur, " %x", mock_data[i]);
+	int target = *bytes_read; /* Maximum we are able to read */
+	int nread = 0;
+	while (target > 0 && out_buffer[-1] != '>') {
+		nread = read(file_des, out_buffer, 1 );
+		out_buffer += nread;
+		target -= nread;
+		DEBUG_DETAIL("Read:", out_buffer - nread);
 	}
 
-	*cur = '\0';
-	*bytes_read = (int)(cur - out_buffer);
+	*bytes_read -= target;
+
 	return OBDII_SUCCESS;
 }
 
+static int is_number_start(char c)
+{
+	return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F');
+}
 
 obdii_result_t obdii_parse(struct pid_response * out, int * n_out,
 			   char * src, int src_len)
@@ -65,27 +248,28 @@ obdii_result_t obdii_parse(struct pid_response * out, int * n_out,
 	 * src should look like:
 	 * 41 0A BE 0F 1C
 	 */
-
-	/* Grab and NUL-terminate PID. */
+	char pid_buf[] = {src[2], src[3], '\0'}; /* Get PID from echo */
 	int accum = 0;
-	char * tok = strtok(src, " ");
-	tok = strtok(NULL, " "); /* Pass over first byte. */
-	out->pid = (char) (strtol(tok, NULL, 16) & 0xFF);
 
+	char * tok = strtok(src, " ");
+	tok = strtok(NULL, " "); /* Pass over first 2 tokens. */
+	tok = strtok(NULL, " ");
 	/* Convert payload from string to int */
-	while (tok) {
+	while (tok && is_number_start(tok[0])) {
+		DEBUG_DETAIL("Token parsed:", tok);
 		accum = accum << 8;
 		accum |= (int) (strtol(tok, NULL, 16) & 0xFF);
 		tok = strtok(NULL, " ");
 	}
 
+	out->pid = (char) (strtol(pid_buf, NULL, 16) & 0xFF);
 	out->value = accum;
 
 	return OBDII_SUCCESS;
 }
 
 /*
- * Encodes each pid as JSON with the schema:
+ * Encodes each pid as JSON with the schema, e.g:
  *
  * {
  *     "17" : 97;
@@ -134,12 +318,18 @@ obdii_result_t obdii_return(char * src_buf)
 void obdii_simple_get_response(char * pids, int n_pids)
 {
 	struct pid_response obdii_data[n_pids];
-	char read_buffer[50] = { 0 };
 	char * encoded = NULL;  /* Points to char buffer.  */
-	int used = 50;		/* Out param for bytes read / data parsed */
-	int i;
 
+#ifdef DEBUG
+	time_t start, end;
+	time(&start);
+#endif
+
+	int i;
 	for (i = 0; i < n_pids; i++) {
+		char read_buffer[50] = { 0 };
+		int used = sizeof(read_buffer) / sizeof(read_buffer[0]);
+
 		obdii_request(0x01, pids[i]);
 		obdii_read(read_buffer, &used);
 		obdii_parse(&obdii_data[i], &used, read_buffer, used);
@@ -148,4 +338,9 @@ void obdii_simple_get_response(char * pids, int n_pids)
 	encoded = obdii_encode(encoded, -1, obdii_data, n_pids);
 	obdii_return(encoded);
 	free(encoded);
+
+#ifdef DEBUG
+	time(&end);
+	fprintf(stderr, "Total execution time: %ld seconds\n", end - start);
+#endif
 }
