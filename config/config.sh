fea4480412b0b641095fbc103ab52d939cc422ab
ngraves95 <ngraves95@gmail.com>
2015-12-26 16:10:24 -0500

    Change taret user to www-data

--- config/config.sh
+++ config/config.sh
@@ -3,5 +3,7 @@
 # This script contains all the configuration needed for the target RPi.
 # It will most likely need to be run with root privileges.
 
-# This adds the user 'pi' to the group 'dialout', which owns the OBD-II device.
-adduser pi dialout
+# Add user 'www-data' to the group 'dialout', which owns the OBD-II device.
+# This allows php to read and write to the device. Unfortunately, the current
+# user must log out and log back in after making this change.
+adduser www-data dialout
