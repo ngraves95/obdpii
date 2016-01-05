6e3410fb34385bbd5f7722498dd8638f625dd1d5
ngraves95 <ngraves95@gmail.com>
2016-01-05 16:01:38 -0500

    Decouple index from PHP implementation.
    This change was done to facilitate an easier transition to other UIs, e.g.
    Android App, iOS App, etc.  Now, the data should be fully RESTful and all
    data from the server, beyond a data-less html page, is served asynchronously.

--- web/dashboard.js
+++ web/dashboard.js
@@ -1,6 +1,8 @@
 // This is a global list of PIDs that the user has requested.
 target_pids = [];
+available_pids = {};
 
+// Generates the card that displays data for this pid.
 function generateCard(name, value, units) {
     return '<li class="component" id="' + name + '">'
 	+ '<div class="component-title">' + name + '</div>'
@@ -8,14 +10,17 @@ function generateCard(name, value, units) {
 	+ value + '<br>'+ units+ '</div>'+ '</li>';
 }
 
+// Hides an element.
 function hide(id) {
     document.getElementById(id).style.display = 'none';
 }
 
+// Shows an element.
 function show(id) {
     document.getElementById(id).style.display = 'block';
 }
 
+// Either shows or hides the pid selector dropdown.
 function togglePidSelector() {
     var pidsel = document.getElementById('pidsel');
 
@@ -30,6 +35,8 @@ function resetPidSelector(sel) {
     sel.selectedIndex = 0;
 }
 
+// Registers a pid to fetch information for. The next AJAX request
+// after this pid has been registered will have the information.
 function registerPid() {
     var sel = document.getElementById("pidsel");
     var pid = (sel.options[sel.selectedIndex].value);
@@ -52,13 +59,14 @@ function registerPid() {
 	return;
     }
     // How to get units?
-    var newCard = generateCard(sel.options[sel.selectedIndex].text, "&nbsp;",
-			       sel.options[sel.selectedIndex].innerHTML);
+    var newCard = generateCard(available_pids[pid].name, "&nbsp;",
+			      available_pids[pid].units);
     document.getElementById("content-list").innerHTML += newCard;
     resetPidSelector(sel);
     hide('pidsel');
 }
 
+// Updates information for all registered pids.
 function update() {
     var ajax = new XMLHttpRequest();
     ajax.onreadystatechange = function () {
@@ -74,7 +82,6 @@ function update() {
 					       jsonResponse[key].pid_value,
 					       jsonResponse[key].pid_units);
 		}
-		components += "";
 		document.getElementById("content-list").innerHTML = components;
 	    }
 	    setTimeout(function() { update(); }, 1000);
@@ -92,6 +99,42 @@ function update() {
     var fullUrl =  "/dashboard_update.php" + urlParams;
     console.log(fullUrl);
     ajax.open("GET", fullUrl);
+    ajax.setRequestHeader("Content-type","application/text");
+    ajax.send();
+}
+
+// Creates the option element for the available pids.  This was done to
+// decouple the index page from a PHP implementation and instead focus on
+// being more RESTful.
+function generateOption(pid, name) {
+    return '<option value="' + pid + '">' + name + '</option>';
+}
+
+// Gets the available pids from the server. The json response is of format
+// pid:string -> {name:string, units:string}
+function getAvailablePids() {
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+	if (ajax.readyState == 4) {
+	    if (ajax.status == 200) {
+		// Handle error; probably shouldn't happen for this app.
+		console.log("parsing available pids...");
+		console.log(ajax.responseText);
+		var jsonResponse =JSON.parse(ajax.responseText);
+		// response maps pid -> {name, units}.
+		available_pids = jsonResponse;
+		var options = '<option value=""> -- Please select a PID -- </option>';
+		for (var key in jsonResponse) {
+		    options += generateOption(key, jsonResponse[key].name);
+		}
+
+		document.getElementById("pidsel").innerHTML = options;
+
+	    }
+	}
+    }
+
+    ajax.open("GET", "/dashboard_available_pids.php");
     ajax.setRequestHeader("Content-type","application/text");
     ajax.send();
 }
